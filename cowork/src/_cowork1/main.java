2492e76fd63552ba93415e7743cc9976f15bb05c
seminarNotes <153586145+seminarNotes@users.noreply.github.com>
2024-01-12 11:19:31 +0900

    Add files via upload
    telebot API 활용을 위한 기본 코드

---
+++ cowork/src/_cowork1/main.java
@@ -0,0 +1,15 @@
+package _cowork1;
+
+public class main {
+    public static void main(String[] args) {
+        String token = "NULL";
+        String chatId = "NULL";
+        String message = "메시지 전송 테스트";
+
+        telebot.funcTelegram(token, chatId, message);
+
+
+
+        
+    }
+}
