87a5cd35ed133295ad4d5f1468287b80f8afd956
bseongwook <150785682+bseongwook@users.noreply.github.com>
2024-01-12 15:16:44 +0900

    Update main.java : use weather infomation

--- cowork/src/_cowork1/main.java
+++ cowork/src/_cowork1/main.java
@@ -4,7 +4,13 @@ public class main {
     public static void main(String[] args) {
         String token = "NULL";
         String chatId = "NULL";
-        String message = "메시지 전송 테스트";
+        String message = "실행 오류";
+		try {
+			message = WeatherCrawling.Weather();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
         telebot.funcTelegram(token, chatId, message);
 
